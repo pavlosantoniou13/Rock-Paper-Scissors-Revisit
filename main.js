3f836c6c9f1c68325e1654cb2881031e9ca40db9
pavlosantoniou13 <dinant11133@gmail.com>
2022-12-17 16:56:33 +0200

    addindex.html main.js style.css

---
+++ main.js
@@ -0,0 +1,44 @@
+const choices = ["rock", "paper", "scissors"]
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(i);
+    }
+    
+}
+
+function playRound(){
+const playerSelection = playerChoice();
+const computerSelection = getComputerChoice();
+const winner = checkWinner(playerSelection, computerSelection);
+console.log(winner)
+}
+
+function playerChoice() {
+    let input = prompt("Rock,Paper or Scissors?");
+    while(input == null){
+        input = prompt("Rock,Paper or Scissors?");
+    }
+    input = input.toLowerCase();
+    return input
+}
+
+function checkWinner(choiceP, choiceC) {
+    if (choiceP === choiceC){
+      return "Tie"
+    } else if ((choiceP === 'rock' && choiceC =='scissors') || (choiceP === 'scissors' && choiceC =='paper') || (choiceP === 'paper' && choiceC =='rock') ){
+      return 'You win'
+    } else {
+      return 'You lose'
+    }
+      
+    
+}
+
+function getComputerChoice() {
+    return choices[Math.floor(Math.random()*choices.length)] 
+    
+}
+
+game();
